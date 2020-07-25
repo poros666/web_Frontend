f571ab96c0b0983ab25be2e672d8fba017236db7
poros666 <790543965@qq.com>
2020-07-25 14:23:32 +0800

    公共部分创建

---
+++ daisy/src/components/comm/FloatHelper.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FloatHelper extends Component {
+    render() {
+        return (
+            <div>
+                <h1>this is FloatHelper</h1>
+            </div>
+        )
+    }
+}
---
+++ daisy/src/components/comm/HeaderNav.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HeaderNav extends Component {
+    render() {
+        return (
+            <div>
+                <h1>这是头部导航栏</h1>
+            </div>
+        )
+    }
+}
