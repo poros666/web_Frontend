f426ecf350b0b193931b0627b7557b498c9f7935
poros666 <790543965@qq.com>
2020-07-27 10:37:41 +0800

    初步路由（未集中配置）

--- daisy/src/pages/admin/adminIndex.js
+++ daisy/src/pages/admin/adminIndex.js
@@ -5,6 +5,9 @@ import { Layout, Menu, Breadcrumb } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import '../../style/admin.css'
 import logo from './xiaohua.png'
+import {Route, Link } from 'react-router-dom';
+import { adminRoutes } from '../../routes/index'
+import CompManagement from './compManagement';
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
@@ -27,28 +30,12 @@ export default class AdminIndex extends Component {
                     defaultOpenKeys={['sub1']}
                     style={{ height: '100%', borderRight: 0 }}
                   >
-                    <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                      <Menu.Item key="1">option1</Menu.Item>
-                      <Menu.Item key="2">option2</Menu.Item>
-                      <Menu.Item key="3">option3</Menu.Item>
-                      <Menu.Item key="4">option4</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                      <Menu.Item key="5">option5</Menu.Item>
-                      <Menu.Item key="6">option6</Menu.Item>
-                      <Menu.Item key="7">option7</Menu.Item>
-                      <Menu.Item key="8">option8</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                      <Menu.Item key="9">option9</Menu.Item>
-                      <Menu.Item key="10">option10</Menu.Item>
-                      <Menu.Item key="11">option11</Menu.Item>
-                      <Menu.Item key="12">option12</Menu.Item>
-                    </SubMenu>
+                    <Menu.Item key="1"><Link to="/admin/comp">比赛管理</Link></Menu.Item>
+                    <Menu.Item key="2"><Link to="/admin/dealreport">处理举报</Link></Menu.Item>
+                    <Menu.Item key="3"><Link to="/admin/usermanagement">用户管理</Link></Menu.Item>
                   </Menu>
                 </Sider>
-                <Layout style={{ padding: '16px' }}>
-                  
+                <Layout style={{ padding: '16px' }}>              
                   <Content
                     className="site-layout-background"
                     style={{
@@ -57,7 +44,7 @@ export default class AdminIndex extends Component {
                       minHeight: 280,
                     }}
                   >
-                    Content
+                    <Route path="/admin/comp" component={CompManagement}/>
                   </Content>
                 </Layout>
               </Layout>
