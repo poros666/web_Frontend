b4364f0b63c6647b2542981aef0166c6ac69d558
hengqu4 <529914423@qq.com>
2020-08-04 16:04:40 +0800

    解决冲突

--- daisy/src/components/comm/FloatHelper.js
+++ daisy/src/components/comm/FloatHelper.js
@@ -1,18 +1,6 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import { BackTop, Affix, Button } from 'antd';
-
-// const styleCustService= {
-//     height: 40,
-//     width: 40,
-//     lineHeight: '40px',
-//     borderRadius: 4,
-//     backgroundColor: '#1088e9',
-//     color: '#fff',
-//     textAlign: 'center',
-//     fontSize: 14,
-//   };
+import { BackTop, Affix,Button, Popover } from 'antd'
 
 const styleBackTop = {
     height: 40,
@@ -47,3 +35,43 @@ export default class FloatHelper extends Component {
          );
     }
 }
+
+// const content=(
+//     <div>
+//         <p>Content</p>
+//         <p>Content</p>
+//     </div> 
+//     );
+
+// export default class FloatHelper extends Component {
+//     state = {
+//         visible: false,
+//       };
+    
+//     hide = () => {
+//         this.setState({
+//           visible: false,
+//         });
+//       };
+    
+//     handleVisibleChange = visible => {
+//         this.setState({ visible });
+//       };
+
+//     render() {
+//         return (
+//             <Affix offsetTop={300} onChange={affixed => console.log(affixed)}>
+//                 <Popover
+//                     content={content}
+//                     placement="rightTop"
+//                     getPopupContainer={triggerNode => triggerNode.parentNode}
+//                     trigger="click"
+//                     visible={this.state.visible}
+//                     onVisibleChange={this.handleVisibleChange}
+//                 >
+//                     <Button type="primary">Click me</Button>
+//                 </Popover>
+//             </Affix>
+//         )
+//     }
+// }
