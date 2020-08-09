217068b772b356689a3cfa96444b994ae9644312
Rdc <1196128112@qq.com>
2020-08-09 16:18:48 +0800

    找回被忽略的文件

---
+++ daisy/src/pages/competition/allCompetitionPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Header from '../../components/comm/HeaderNav'
+import Footer from '../../components/comm/Footer'
+import Helper from '../../components/comm/FloatHelper'
+import SortChoose from '../../components/competition/sortChoose'
+import CompetitionList from '../../components/competition/competitionList'
+import { Col} from 'antd'
+import 'antd/dist/antd.css'
+
+export default class AllCompetitionPage extends Component {
+    render(){
+        return (
+            <div class='allCompetitionPage'>
+                <Header/>
+                <Helper/>
+                <Col offset={2} span={20}>
+                <SortChoose/>
+                <CompetitionList/>
+                </Col>
+                <Footer/>
+            </div>
+        )
+    }
+}
