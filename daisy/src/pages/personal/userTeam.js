cb355f0313866112244f56bd771b44cdaaf1646e
Rdc <1196128112@qq.com>
2020-09-18 23:36:48 +0800

    暂时写完，未测试

--- daisy/src/pages/personal/userTeam.js
+++ daisy/src/pages/personal/userTeam.js
@@ -1,84 +1,20 @@
 import React, { Component } from 'react'
-import {Card,List,Avatar} from 'antd'
+import {Card,List} from 'antd'
 import {EditOutlined} from '@ant-design/icons'
 import { Link } from 'react-router-dom'
-
-const teamData = [
-    {
-        teamID:1,
-        compID:1,
-        teamname:'team 1',
-        limit:10,
-        compname: 'comp 1',
-        description:
-          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-        member:[
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-        ]
-    },
-    {
-        teamID:2,
-        compID:2,
-        teamname:'team 2',
-        limit:7,
-        compname: 'comp 2',
-        description:
-          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-        member:[
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-        ]
-    },
-    {
-        teamID:3,
-        compID:3,
-        teamname:'team 3',
-        limit:5,
-        compname: 'comp 3',
-        description:
-          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-          member:[
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-        ]
-    },
-    {
-        teamID:4,
-        compID:4,
-        teamname:'team 4',
-        limit:3,
-        compname: 'comp 4',
-        description:
-          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-          member:[
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
-            "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-        ]
-    }]
-
 
 export default class UserTeam extends Component {
     constructor(props){
         super(props)
         this.state={
-          data:teamData
+          data:[]
         }
         var token=JSON.parse( localStorage.getItem('token')).token
         Axios.get('/Usergroups/'+this.props.account,{headers: { "Authorization": 'Bearer ' +token }})
         .then((res)=>{
-            res.data//这里是team数据
+            this.setState({
+                data:res.data,
+            })
         })
         .catch(function(error){
             console.log(error)
@@ -98,44 +34,22 @@ export default class UserTeam extends Component {
                         title={item.teamname}
                         extra={<div>
                                 <Link to={{pathname:"#/editteam/"+item.teamID,
-                                            query:{/*这里放比赛id队伍id名字和简介*/}}}>
+                                            query:{
+                                                GroupId:item.groupId,
+                                                ProjectId:item.projectId,
+                                            }}}>
                                     <EditOutlined/>
                                     </Link>
                                   </div>
                         }>
                             <p>
-                                <a href={"#/compPage/"+item.compID+"=id"+item.compID}>
-                                    {item.compname}
-                                </a>
+                                简介：{item.introduction}
                             </p>
-                            <p>
-                                最多人数{item.limit}
-                            </p>
-                            <p>
-                                简介：{item.description}
-                            </p>
-                            <Avatar.Group maxCount={6}>
-                                {
-                                    item.member.map((item,index)=>{
-                                        return <Avatar src={item} key={index}/>
-
-                                    })
-                                }
-                            </Avatar.Group>
                         </Card>
                     </List.Item>
                 )}
                 />
             </div>
         )
-    }
-    handleChangePage(teamID)
-    {
-      this.context.router.push(
-        {
-          path:'#/team/'+teamID,
-          ID:teamID
-        }
-      )
     }
 }
