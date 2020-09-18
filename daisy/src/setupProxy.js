9ffc20b7e4721b70b58e2c237bea380b067ea7d8
poros666 <44796985+poros666@users.noreply.github.com>
2020-09-18 19:22:54 +0800

    可以运行了

--- daisy/src/setupProxy.js
+++ daisy/src/setupProxy.js
@@ -2,21 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-<<<<<<< HEAD
     '/api',
     createProxyMiddleware({
       target: 'http://49.234.209.5:8080',
       changeOrigin: true
     })
-=======
-    "/api",
-    createProxyMiddleware({
-    target: '49.234.209.5:8080',
-    changeOrigin: true,
-    pathRewrite: {
-      "^/api": "",
-    }
-  })
->>>>>>> community
   )
 }
