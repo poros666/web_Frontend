f8fcfb82cd261742fd9da92fae5773a13ea10dae
poros666 <44796985+poros666@users.noreply.github.com>
2020-09-18 20:31:43 +0800

    暂存 差500的问题

--- daisy/src/components/community/ReadMoment.js
+++ daisy/src/components/community/ReadMoment.js
@@ -2,178 +2,168 @@
 // made by ykn
 //
 
-
-//记录明天的部分 
+//记录明天的部分
 //排序
 //登陆状态的用户体验提升
 
-
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
-import { Card,Avatar,Space,Button } from 'antd'
+import { Card, Avatar, Space, Button } from 'antd'
 import '../../style/comm/comm.css'
-import { LikeOutlined, StarOutlined,CommentOutlined } from '@ant-design/icons'
+import { LikeOutlined, StarOutlined, CommentOutlined } from '@ant-design/icons'
 import ReportButton from './ReportButton'
 import CONSTURL from './config'
 import Axios from 'axios'
 import Loading from './Loading'
 //import Report from '../findTeammate/report'
 import moment from 'moment'
-import {isLogined} from '../../utils/auth'
+import { isLogined } from '../../utils/auth'
 
-Axios.defaults.baseURL='/api'
-
-
+Axios.defaults.baseURL = '/api'
 
 const IconText = ({ icon, text }) => (
-    <Space>
-      {React.createElement(icon)}
-      {text}
-    </Space>
-  );
-  
-
+  <Space>
+    {React.createElement(icon)}
+    {text}
+  </Space>
+)
 
 export default class ReadMoment extends Component {
-    
+  constructor(props) {
+    super()
 
-    constructor(props){
-        super()
-        
-        this.state={
-            data:{
-                "Icon":"",
-                "Account":"",
-                "Nickname":"",
-                "Title":"",
-                "Content":"",
-                "Time":"",
-                "LikeCount":0,
-                "CommentCount":0,
-                "StarCount":0
-            },
-            isLoading:true,
-            Mid:props.momentId,
-        }
-
-        this.getMomentContent(this.state.Mid)
+    this.state = {
+      data: {
+        Icon: '',
+        Account: '',
+        Nickname: '',
+        Title: '',
+        Content: '',
+        Time: '',
+        LikeCount: 0,
+        CommentCount: 0,
+        StarCount: 0,
+      },
+      isLoading: true,
+      Mid: props.momentId,
     }
 
-    componentDidMount(){
-       // console.log("123123131312")
-        this.getMomentContent(this.state.Mid)
-    }
-    
+    this.getMomentContent(this.state.Mid)
+  }
 
-    getMomentContent(Mid){
-        //do something
-        var url=CONSTURL.GetMoment+Mid
-        Axios.get(url).then((res)=>{
-            console.log(res)
-            this.setState({data:res.data})
-            this.setState({isLoading:false})
-        })
+  componentDidMount() {
+    // console.log("123123131312")
+    this.getMomentContent(this.state.Mid)
+  }
+
+  getMomentContent(Mid) {
+    //do something
+    var url = CONSTURL.GetMoment + Mid
+    Axios.get(url).then((res) => {
+      console.log(res)
+      this.setState({ data: res.data })
+      this.setState({ isLoading: false })
+    })
+  }
+
+  likeMoment() {
+    if (isLogined()) {
+      var json = {
+        MomentId: Number(this.state.Mid),
+        Account: 'ddd',
       }
+      var url = CONSTURL.LikeMoment
 
-    likeMoment(){
-    if(isLogined()){
-        var json=
-            {
-                "MomentId":this.state.Mid,
-                "Account":'ddd'
-            }
-        var url=CONSTURL.LikeMoment
-
-        Axios.post(url,json).then((res)=>{
-            console.log(res)
-            window.location.reload()
-        })
+      var token = JSON.parse(localStorage.getItem('token')).token
+      Axios.post(url, json, {
+        headers: { Authorization: 'Bearer ' + token },
+      }).then((res) => {
+        window.location.reload()
+      })
+    } else {
+      window.alert('未登录，跳转至登陆界面')
+      window.location.hash = '#/login'
     }
-    else{
-        window.alert("未登录，跳转至登陆界面")
-        window.location.hash='#/login'
+  }
+
+  starMoment() {
+    if (isLogined()) {
+      var json = {
+        MomentId: Number(this.state.Mid),
+        Account: JSON.parse(localStorage.userData).account.toString(),
+        Name: 'Moment',
       }
+      var url = CONSTURL.StarMoment
+      var token = JSON.parse(localStorage.getItem('token')).token
+      Axios.post(url, json, {
+        headers: { Authorization: 'Bearer ' + token },
+      }).then((res) => {
+        window.location.reload()
+      })
+    } else {
+      window.alert('未登录，跳转至登陆界面')
+      window.location.hash = '#/login'
     }
+  }
 
-    starMoment(){
-        if(isLogined()){
-            var json=
-            {
-                "MomentId":this.state.Mid,
-                "Account":"ddd",
-                "Name":"Moment"
-            }
-            var url=CONSTURL.StarMoment
-            Axios.post(url,json).then((res)=>{
-                console.log(res)
-                window.location.reload()
-            })
-        }
-        else{
-            window.alert("未登录，跳转至登陆界面")
-            window.location.hash='#/login'
-        }
+  render() {
+    return this.state.isLoading ? (
+      <Loading />
+    ) : (
+      <div className='site-card-border-less-wrapper'>
+        <Card
+          title={this.state.data.Title}
+          bordered={false}
+          extra={
+            //之后可以用button之类的包装一下做成超链接
+            //这里的头像要动态生成
+            <div align='right'>
+              <a href={'#/personal'}>
+                <Avatar src={this.state.data.Icon}></Avatar>
+              </a>
 
-    }
-
-
-      
-    render() {
-        return (
-                this.state.isLoading ? <Loading /> :
-                <div className='site-card-border-less-wrapper'>
-                <Card
-                    title={this.state.data.Title}
-                    bordered={false} 
-                    extra={//之后可以用button之类的包装一下做成超链接
-                        //这里的头像要动态生成
-                        <div align="right">
-
-                        <a href={"#/personal"}>
-                            <Avatar src={this.state.data.Icon}></Avatar>
-                        </a>
-
-                        <p>{this.state.data.Nickname}</p>
-
-                        </div>
-                        }
-                    actions={[
-                        <Button type='text' onClick={this.starMoment.bind(this)}>
-                            <IconText icon={StarOutlined} text={this.state.data.StarCount} key="list-vertical-star-o" />
-                        </Button>,
-
-                        <Button type='text' onClick={this.likeMoment.bind(this)}>
-                            <IconText icon={LikeOutlined} text={this.state.data.LikeCount} key="list-vertical-like-o" />
-                        </Button>,
-
-                        <Button type='text'>
-                            <IconText icon={CommentOutlined} text={this.state.data.CommentCount} key="list-vertical-share-o" />
-                        </Button>,
-
-                        <ReportButton
-                            ReportUID={this.state.Mid} 
-                            ReporterUID='test2' 
-                            Time={moment().format("YYYY-MM-DDTHH:mm:ssC")}
-                            ContentType="moment"
-                        />,
-
-                        ]}
-                    >
-                {
-                    //下面是帖子的内容部分
-                }
-                <p>
-                   { this.state.data.Content}
-                </p>
-                </Card>
-                <br/>
+              <p>{this.state.data.Nickname}</p>
             </div>
-            
+          }
+          actions={[
+            <Button type='text' onClick={this.starMoment.bind(this)}>
+              <IconText
+                icon={StarOutlined}
+                text={this.state.data.StarCount}
+                key='list-vertical-star-o'
+              />
+            </Button>,
 
-            
-        )
-    }
+            <Button type='text' onClick={this.likeMoment.bind(this)}>
+              <IconText
+                icon={LikeOutlined}
+                text={this.state.data.LikeCount}
+                key='list-vertical-like-o'
+              />
+            </Button>,
+
+            <Button type='text'>
+              <IconText
+                icon={CommentOutlined}
+                text={this.state.data.CommentCount}
+                key='list-vertical-share-o'
+              />
+            </Button>,
+
+            <ReportButton
+              ReportUID={this.state.Mid}
+              ReporterUID='test2'
+              Time={moment().format('YYYY-MM-DDTHH:mm:ssC')}
+              ContentType='moment'
+            />,
+          ]}>
+          {
+            //下面是帖子的内容部分
+          }
+          <p>{this.state.data.Content}</p>
+        </Card>
+        <br />
+      </div>
+    )
+  }
 }
-
-
-
--- daisy/src/components/community/ReportButton.js
+++ daisy/src/components/community/ReportButton.js
@@ -1,21 +1,27 @@
-import React, { useState,Component } from "react"
-import { Modal, Form, Input, Select,Popover } from "antd"
-import { WarningOutlined} from "@ant-design/icons"
+import React, { useState, Component } from 'react'
+import { Modal, Form, Input, Select, Popover } from 'antd'
+import { WarningOutlined } from '@ant-design/icons'
 import axios from 'axios'
-import { isLogined } from "../../utils/auth";
+import { isLogined } from '../../utils/auth'
 
-
-const { Option } = Select;
+const { Option } = Select
 
 //添加举报的弹出框
-const CollectionCreateForm = ({ visible, onCreate, onCancel,ReportUID,ReporterUID,Time}) => {
+const CollectionCreateForm = ({
+  visible,
+  onCreate,
+  onCancel,
+  ReportUID,
+  ReporterUID,
+  Time,
+}) => {
   const [form] = Form.useForm()
   return (
     <Modal
       visible={visible}
-      title="创建举报单"
-      okText="确定"
-      cancelText="取消"
+      title='创建举报单'
+      okText='确定'
+      cancelText='取消'
       onCancel={onCancel}
       onOk={() => {
         form
@@ -25,53 +31,45 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel,ReportUID,ReporterUI
             onCreate(values)
           })
           .catch((info) => {
-            console.log("Validate Failed:", info)
+            console.log('Validate Failed:', info)
           })
-      }}
-    >
+      }}>
       <Form
         form={form}
-        layout="vertical"
-        name="report_form_in_modal"
+        layout='vertical'
+        name='report_form_in_modal'
         initialValues={{
-          tags: "not_started",
-        }}
-      >
-        <Form.Item
-          name="time"
-          label="举报时间"
-        >
-          <Input placeholder={Time} disabled/>
+          tags: 'not_started',
+        }}>
+        <Form.Item name='time' label='举报时间'>
+          <Input placeholder={Time} disabled />
+        </Form.Item>
+        <Form.Item name='reporter_id' label='您的用户id'>
+          <Input placeholder={ReporterUID} disabled />
+        </Form.Item>
+        <Form.Item name='target_id' label='被举报帖子id'>
+          <Input placeholder={ReportUID} disabled />
         </Form.Item>
         <Form.Item
-          name="reporter_id"
-          label="您的用户id"
-        >
-          <Input placeholder={ReporterUID} disabled/>
-        </Form.Item>
-        <Form.Item
-          name="target_id"
-          label="被举报帖子id"
-        >
-          <Input placeholder={ReportUID} disabled/>
-        </Form.Item>
-        <Form.Item 
-          name="types" label="举报类型" 
+          name='types'
+          label='举报类型'
           rules={[{ required: true, message: '请选择举报类型' }]}>
-          <Select initialValues="色情" style={{ width: 120 }}>
-            <Option value="sex">色情</Option>
-            <Option value="policy">涉政</Option>
-            <Option value="effect">影响他人</Option>
-            <Option value="trade">涉及交易</Option>
-            <Option value="spite">恶意</Option>
+          <Select initialValues='色情' style={{ width: 120 }}>
+            <Option value='sex'>色情</Option>
+            <Option value='policy'>涉政</Option>
+            <Option value='effect'>影响他人</Option>
+            <Option value='trade'>涉及交易</Option>
+            <Option value='spite'>恶意</Option>
           </Select>
         </Form.Item>
-        <Form.Item name="description" label="举报内容" 
-        rules={[{ required: true, message: '请填写举报内容' }]}>
+        <Form.Item
+          name='description'
+          label='举报内容'
+          rules={[{ required: true, message: '请填写举报内容' }]}>
           <Input.TextArea
             allowClear={true}
             autoSize={{ minRows: 1, maxRows: 30 }}
-            placeholder="在此输入举报原因等详情"
+            placeholder='在此输入举报原因等详情'
           />
         </Form.Item>
       </Form>
@@ -80,39 +78,46 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel,ReportUID,ReporterUI
 }
 
 //调用按钮
-const CollectionsPageReport = ({ReporterUID,ReportUID,Time,ContentType}) => {
+const CollectionsPageReport = ({
+  ReporterUID,
+  ReportUID,
+  Time,
+  ContentType,
+}) => {
   const [visible, setVisible] = useState(false)
 
   const onCreate = (values) => {
-    values.time=Time;
-    values.reporter_id=ReporterUID;
-    values.target_id=ReportUID;
-    values.t_type=ContentType;
-    console.log("Received values of form: ", values)
-    if(isLogined()){
-    let dataSent={
-      Account:values.reporter_id,
-      ReportType:values.types,
-      Content:values.description,
-      Time:values.time,
-      TargetType:values.t_type,
-      TargetId:values.target_id
-    }
- //   console.log(dataSent)
-    if(dataSent.Account.length>0){
-    axios.post('http://mock-api.com/5g7AeqKe.mock/Report',dataSent)
-        .then(response=>{
-          console.log(response)
-          window.alert("举报成功")
-        })
+    values.time = Time
+    values.reporter_id = ReporterUID
+    values.target_id = ReportUID
+    values.t_type = ContentType
+    console.log('Received values of form: ', values)
+    if (isLogined()) {
+      let dataSent = {
+        Account: values.reporter_id,
+        ReportType: values.types,
+        Content: values.description,
+        Time: values.time,
+        TargetType: values.t_type,
+        TargetId: Number(values.target_id) ,
       }
-      else{
-        window.alert("举报失败")
+      console.log(dataSent)
+      if (dataSent.Account.length > 0) {
+        var token = JSON.parse(localStorage.getItem('token')).token
+        axios
+          .post('/Report', dataSent, {
+            headers: { Authorization: 'Bearer ' + token },
+          })
+          .then((response) => {
+            console.log(response)
+            window.alert('举报成功')
+          })
+      } else {
+        window.alert('举报失败')
       }
-    }
-    else{
-      window.alert("未登录，跳转至登陆界面")
-      window.location.hash='#/login'
+    } else {
+      window.alert('未登录，跳转至登陆界面')
+      window.location.hash = '#/login'
     }
     //处理数据
     setVisible(false)
@@ -139,11 +144,15 @@ const CollectionsPageReport = ({ReporterUID,ReportUID,Time,ContentType}) => {
   )
 }
 
-
 export default class ReportButton extends Component {
   render() {
     return (
-        <CollectionsPageReport ReportUID={this.props.ReportUID} ReporterUID={this.props.ReporterUID} Time={this.props.Time} ContentType={this.props.ContentType}/>
+      <CollectionsPageReport
+        ReportUID={this.props.ReportUID}
+        ReporterUID={this.props.ReporterUID}
+        Time={this.props.Time}
+        ContentType={this.props.ContentType}
+      />
     )
   }
 }
