9fe6ceded83d07af2e655b21971bae6ac0c9d2a1
hengqu4 <529914423@qq.com>
2020-09-18 16:33:36 +0800

    axios接口

--- daisy/src/pages/Homepage/CommunityShow.js
+++ daisy/src/pages/Homepage/CommunityShow.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import 'antd/dist/antd.css'
 import { Layout, Button, List, Avatar,Space, Row, Col } from 'antd'
 import { MessageOutlined, LikeOutlined, StarOutlined} from '@ant-design/icons'
+import CONSTURL from './config'
+import Axios from 'axios';
 import '../../style/homepage.css'
 
 const IconText = ({ icon, text }) => (
@@ -16,63 +18,6 @@ const count = 3;
 const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat&noinfo`;
 const { Header, Footer, Sider, Content } = Layout;
  
-const sourceData = [
-    {
-    Uid:111,
-    Pid:11,
-    name:'程序员小昭',
-    time:'2020-09-09',
-    avatarSrc:'bear',
-    title: '一文看懂前端和后端开发:从入门到放弃',
-    description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
-    },
-    {
-    Uid:112,
-    Pid:12,
-    name:'程序员小李',
-    time:'2020-09-09',
-    avatarSrc:'pig',
-    title: 'Linux网络数据转发平面的变迁-从内核协议栈到DPDK/XDP',
-    description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
-
-    },
-    {
-    Uid:113,
-    Pid:13,
-    name:'程序员小王',
-    time:'2020-09-09',
-    avatarSrc:'roo',
-    title: '【数据结构与算法】详解什么是图结构，并用代码手动实现一个图结构',
-    description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
-
-    },
-    {
-    Uid:114,
-    Pid:15,
-    avatarSrc:'eeyore',
-    name:'程序员小宋',
-    time:'2020-09-09',
-    title: 'OpenCV-Python图像转换为PyQt图像的变形及花屏无法正常显示问题研究',
-    description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
-    },
-    {
-        Uid:116,
-        Pid:16,
-        avatarSrc:'roo',
-        name:'程序员小王',
-        time:'2020-09-09',
-        title: '快速学懂Lingo软件及其编程方法',
-        description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
-    },
-    // {
-    // Uid:115,
-    // Pid:17,
-    // avatarSrc:'strange',
-    // title: '数据库的乐观锁和悲观锁并非真实的锁',
-    // description:'Ant Design, a design language for '
-    // },
-];
-
 function limitTxt(txt,count) {
     var str = txt;
     if(txt.length>count){
@@ -84,9 +29,61 @@ function limitTxt(txt,count) {
 class CommunityShow extends Component {
     constructor(props){
         super(props)
+        
+        var sourceData = [
+            {
+            Uid:111,
+            Pid:11,
+            name:'程序员小昭',
+            time:'2020-09-09',
+            avatarSrc:'bear',
+            title: '一文看懂前端和后端开发:从入门到放弃',
+            description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
+            },
+            {
+            Uid:112,
+            Pid:12,
+            name:'程序员小李',
+            time:'2020-09-09',
+            avatarSrc:'pig',
+            title: 'Linux网络数据转发平面的变迁-从内核协议栈到DPDK/XDP',
+            description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
+        
+            },
+            {
+            Uid:113,
+            Pid:13,
+            name:'程序员小王',
+            time:'2020-09-09',
+            avatarSrc:'roo',
+            title: '【数据结构与算法】详解什么是图结构，并用代码手动实现一个图结构',
+            description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
+        
+            },
+            {
+            Uid:114,
+            Pid:15,
+            avatarSrc:'eeyore',
+            name:'程序员小宋',
+            time:'2020-09-09',
+            title: 'OpenCV-Python图像转换为PyQt图像的变形及花屏无法正常显示问题研究',
+            description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
+            },
+            {
+                Uid:116,
+                Pid:16,
+                avatarSrc:'roo',
+                name:'程序员小王',
+                time:'2020-09-09',
+                title: '快速学懂Lingo软件及其编程方法',
+                description:'Ant Design, a design language for background applications, is refined by Ant UED Team'
+            },
+        ];
 
         this.state={
-            data:sourceData
+            // data:sourceData,
+            currentData:[],
+            isLoaded:false,
         }
     }
 
@@ -94,8 +91,30 @@ class CommunityShow extends Component {
 
     }
 
-    render() {
+    componentDidMount(){
+        const _this=this;    //先存一下this，以防使用箭头函数this会指向我们不希望它所指向的对象。
+        Axios.get('http://fwdarling2020.cn:8080/api/Moment/Random')
+        .then(function (response) {
+          _this.setState({
+            currentData:response.data,
+            isLoaded:true
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+          _this.setState({
+            isLoaded:false,
+            error:error
+          })
+        })
+    }
 
+    render() {
+        // if(!this.state.isLoaded){
+        //     return <div>Loading</div>
+        // }
+        // else{
+            if(1){
         return ( 
             // <div style={{height: '400px',width:'600px',margin:'10px 10px',float:'right'}}>
             <div style={{height: '100%',margin:'10px'}}>
@@ -127,7 +146,7 @@ class CommunityShow extends Component {
                         <List
                             // bordered={true}
                             itemLayout="horizontal"
-                            dataSource={this.state.data}
+                            dataSource={this.state.currentData}
                             renderItem={item => (
                             <List.Item>
 
@@ -170,6 +189,7 @@ class CommunityShow extends Component {
                 </Layout>
             </div>
          );
+      }
     }
 }
  
